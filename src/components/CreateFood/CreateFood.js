94f5126fea81417c580aedefca89583c06c5aae0
Mohammad Biplob Molla <ahmedtamim317@gmail.com>
2022-12-27 23:16:45 +0600

    Done

---
+++ src/components/CreateFood/CreateFood.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Button, Col, Form, FormGroup, Input, Label, Row } from 'reactstrap';
+
+function CreateFood() {
+    return (
+        <Row>
+            <Col className='col-md-6 m-auto'>
+                <h1 className='mx-2'>Food Create</h1>
+                <Form className='container-fluid my-5'>
+                    <FormGroup>
+                        <Label for="exampleSelect">Category</Label>
+                        <Input type="select" name="select" id="exampleSelect">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="exampleTitle">Title</Label>
+                        <Input type="text " name="title" id="exampleEmail" placeholder="Title" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="exampleNumber">Prize</Label>
+                        <Input type="number" name="number" id="exampleNumber" placeholder="Prize" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="exampleFile">File</Label>
+                        <Input type="file" name="file" id="exampleFile" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="exampleText">Description</Label>
+                        <Input rows="7" type="textarea" name="description" id="exampleText" />
+                    </FormGroup>
+                    <Button>Create</Button>
+                </Form>
+            </Col>
+        </Row>
+    )
+}
+
+export default CreateFood;
--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Routes, Route, Navigate} from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import Home from "../pages/Home";
 import AllFoods from "../pages/AllFoods";
@@ -9,22 +9,24 @@ import Checkout from "../pages/Checkout";
 import Contact from "../pages/Contact";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import CreateFood from '../components/CreateFood/CreateFood';
 
 
 const Routers = () => {
-    return (
-        <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/foods" element={<AllFoods />} />
-        <Route path="/foods/:id" element={<FoodDetails />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/foods" element={<AllFoods />} />
+      <Route path="/foods/:id" element={<FoodDetails />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/CreateFood" element={<CreateFood />} />
+    </Routes>
+  );
 };
 
 export default Routers;
